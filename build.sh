fd619a05aae78740adca37fa18b95c1482439d48
Chugunov Roman <Zebs-BMK@yandex.ru>
2023-04-05 00:52:16 +0300

    impr: add ci

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cargo build --release
-
-cp -rf ./target/release/font_helper ./res/fonthelper
-
-cd ./res
-
-tar cJf ../fonthelper.tar.xz ./{figma-fonthelper*,fonthelper*,updater.sh,libfreetype*}
