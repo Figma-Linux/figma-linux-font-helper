379e6cdf292560d018b51dcfc75c41240b58a61a
Chugunov Roman <Zebs-BMK@yandex.ru>
2023-04-05 12:21:48 +0300

    fix: build

--- src/log.rs
+++ src/log.rs
@@ -1,16 +1,22 @@
 extern crate flexi_logger;
 extern crate xdg;
 
-use flexi_logger::{opt_format, Logger};
+use flexi_logger::{opt_format, FileSpec, Logger};
 
 pub fn init() {
   let xdg_dirs = xdg::BaseDirectories::with_prefix("figma-fonthelper").unwrap();
 
-  Logger::with_env_or_str(
+  Logger::try_with_env_or_str(
     "font_helper=debug, finder=debug, libfonthelper=debug, simple_server=info",
   )
-  .log_to_file()
-  .directory(xdg_dirs.get_cache_home())
+  .unwrap()
+  .log_to_file(
+    FileSpec::default()
+      .directory(xdg_dirs.get_cache_home())
+      .basename("font-helper")
+      .suppress_timestamp()
+      .suffix("log"),
+  )
   .format(opt_format)
   .start()
   .unwrap();
