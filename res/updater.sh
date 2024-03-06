3ca9490a81da657855cffe8ad1813d1edda3bbd4
Joseph Dalrymple <swivelgames@gmail.com>
2024-03-06 02:32:09 -0600

    fix: improved compatibility with xdg base dir spec and posix bash

--- res/updater.sh
+++ res/updater.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+DATA_DIR=${XDG_DATA_HOME:-$HOME/.local/share}
+CONFIG_DIR=${XDG_CONFIG_HOME:-$HOME/.config}
+APP_DATA_DIR=$DATA_DIR/figma-linux
 
 get_latest_release() {
   curl -Ls --silent "https://github.com/ChugunovRoman/figma-linux-font-helper/releases/latest" | perl -ne 'print "$1\n" if /v([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,4})/' | head -1;
@@ -10,7 +14,7 @@ get_latest_release_link_download() {
 }
 
 have_new_version() {
-  local current=$($HOME/.local/share/figma-fonthelper/fonthelper -v);
+  local current=$($DATA_DIR/figma-fonthelper/fonthelper -v);
   local latest=$(get_latest_release);
 
   if [ ! $current == $latest ]; then
@@ -32,9 +36,6 @@ download() {
 }
 
 install() {
-  DATA_DIR=${XDG_DATA_HOME:-$HOME/.local/share}
-  CONFIG_DIR=${XDG_CONFIG_HOME:-$HOME/.config}
-  APP_DATA_DIR=$DATA_DIR/figma-linux
 
   pushd $APP_DATA_DIR
   tar xJf /tmp/fonthelper.tar.xz ./fonthelper
@@ -43,8 +44,8 @@ install() {
   popd
 
   pushd $CONFIG_DIR/systemd/user
-  tar xJOf /tmp/fonthelper.tar.xz ./figma-fonthelper.service | XDG_CONFIG_HOME=$DATA_DIR envsubst > figma-fonthelper.service
-  tar xJOf /tmp/fonthelper.tar.xz ./figma-fonthelper-updater.service | XDG_CONFIG_HOME=$DATA_DIR envsubst > figma-fonthelper-updater.service
+  tar xJOf /tmp/fonthelper.tar.xz ./figma-fonthelper.service
+  tar xJOf /tmp/fonthelper.tar.xz ./figma-fonthelper-updater.service
 
   chmod 644 figma-fonthelper.service
   chmod 644 figma-fonthelper-updater.service
